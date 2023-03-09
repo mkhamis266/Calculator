c237f63b5938284da06a21f5c183e9c6828d899c
Mahmoud Khamis Ahmed <68229221+mkhamis266@users.noreply.github.com>
2023-03-09 10:50:27 +0200

    ..

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,20 +7,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   result = '';
+  new_operation = false;
 
   getAction(item:string|number) {
-    if (item ==="DEL"){
-      this.result = this.result.slice(0,-1);
-      return;
+    if (this.new_operation == false){
+      if (item ==="DEL"){
+        this.result = this.result.slice(0,-1);
+        return;
+      }
+    } else if (this.new_operation == true && (item !== "+" && item !== "-" && item != "*" && item !== "/") ) {
+        this.result = item.toString();
+        this.new_operation = false;
+        return
     }
     this.result += item; 
+    this.new_operation = false;
   }
 
   clrear() {
     this.result = ''
+    this.new_operation = false;
   }
 
   equal(){
-    this.result = eval(this.result)
+    this.result = eval(this.result).toFixed(2);
+    this.new_operation = true;
   }
 }
